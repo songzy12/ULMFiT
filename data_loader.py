50beff0d24c7e0d0cc89c985a20b5bd2a505ca1b
Zhengyang Song <songzy_thu@163.com>
2022-09-29 13:57:00 +0800

    Refactor create_data_loader_for_lm.

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,13 @@
-import numpy as np
+from functools import partial
 
+import numpy as np
 import paddle
 
 from paddlenlp.datasets import load_dataset
 from paddlenlp.data import Vocab
 
 
-def create_data_loader_for_lm(batch_size, num_steps):
-    train_ds, valid_ds, test_ds = load_dataset('ptb',
-                                               splits=('train', 'valid',
-                                                       'test'))
-
-    train_examples = [
-        train_ds[i]['sentence'].split() for i in range(len(train_ds))
-    ]
-    vocab = Vocab.build_vocab(train_examples, eos_token='</eos>')
-
+def convert_ptb_example(examples, vocab, batch_size, num_steps):
     # Because the sentences in PTB dataset might be consecutive, we need to concatenate
     # all texts from our dataset and fold them into chunks while the number of rows is
     # equal to batch size. For example:
@@ -29,32 +21,43 @@ def create_data_loader_for_lm(batch_size, num_steps):
     #                  ["of", "asbestos", "having", "any"],
     #                  ["questionable", "properties", "there", "is"],
     #                  ["no", "asbestos", "in", "our"]]
-    #
-    def group_texts(examples):
-        concat_examples = []
-        for example in examples:
-            concat_examples += example['sentence'].split() + ['</eos>']
+    concat_examples = []
+    for example in examples:
+        concat_examples += example['sentence'].split() + ['</eos>']
 
-        concat_examples = vocab.to_indices(concat_examples)
+    concat_examples = vocab.to_indices(concat_examples)
 
-        max_seq_len = len(concat_examples) // batch_size
-        reshaped_examples = np.asarray(concat_examples[0:batch_size *
-                                                       max_seq_len],
-                                       dtype='int64').reshape(
-                                           (batch_size, max_seq_len))
-        encoded_examples = []
-        for i in range(max_seq_len // num_steps):
-            encoded_examples.append(
-                (np.copy(reshaped_examples[:,
-                                           i * num_steps:(i + 1) * num_steps]),
-                 np.copy(reshaped_examples[:, i * num_steps +
-                                           1:(i + 1) * num_steps + 1])))
+    max_seq_len = len(concat_examples) // batch_size
+    reshaped_examples = np.asarray(concat_examples[0:batch_size * max_seq_len],
+                                   dtype='int64').reshape(
+                                       (batch_size, max_seq_len))
+    encoded_examples = []
+    for i in range(max_seq_len // num_steps):
+        encoded_examples.append(
+            (np.copy(reshaped_examples[:, i * num_steps:(i + 1) * num_steps]),
+             np.copy(reshaped_examples[:, i * num_steps +
+                                       1:(i + 1) * num_steps + 1])))
 
-        return encoded_examples
+    return encoded_examples
 
-    train_ds.map(group_texts, batched=True)
-    valid_ds.map(group_texts, batched=True)
-    test_ds.map(group_texts, batched=True)
+
+def create_data_loader_for_lm(batch_size, num_steps):
+    train_ds, valid_ds, test_ds = load_dataset('ptb',
+                                               splits=('train', 'valid',
+                                                       'test'))
+
+    train_examples = [
+        train_ds[i]['sentence'].split() for i in range(len(train_ds))
+    ]
+    vocab = Vocab.build_vocab(train_examples, eos_token='</eos>')
+
+    trans_fn = partial(convert_ptb_example,
+                       vocab=vocab,
+                       batch_size=batch_size,
+                       num_steps=num_steps)
+    train_ds.map(trans_fn, batched=True)
+    valid_ds.map(trans_fn, batched=True)
+    test_ds.map(trans_fn, batched=True)
 
     train_loader = paddle.io.DataLoader(train_ds,
                                         return_list=True,
