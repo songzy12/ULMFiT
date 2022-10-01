896394c40e678798def05eed1f34a51f4a75f25b
Zhengyang Song <songzy_thu@163.com>
2022-10-02 00:11:32 +0800

    Add data loader for tc and its unit test.

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,7 @@ import numpy as np
 import paddle
 
 from paddlenlp.datasets import load_dataset
-from paddlenlp.data import Vocab
+from paddlenlp.data import Vocab, Pad, Stack, Tuple
 
 
 def convert_ptb_example(examples, vocab, batch_size, num_steps):
@@ -28,44 +28,84 @@ def convert_ptb_example(examples, vocab, batch_size, num_steps):
     concat_examples = vocab.to_indices(concat_examples)
 
     max_seq_len = len(concat_examples) // batch_size
-    reshaped_examples = np.asarray(concat_examples[0:batch_size * max_seq_len],
-                                   dtype='int64').reshape(
-                                       (batch_size, max_seq_len))
+    reshaped_examples = np.asarray(
+        concat_examples[0:batch_size * max_seq_len], dtype='int64').reshape(
+            (batch_size, max_seq_len))
     encoded_examples = []
     for i in range(max_seq_len // num_steps):
         encoded_examples.append(
             (np.copy(reshaped_examples[:, i * num_steps:(i + 1) * num_steps]),
-             np.copy(reshaped_examples[:, i * num_steps +
-                                       1:(i + 1) * num_steps + 1])))
+             np.copy(reshaped_examples[:, i * num_steps + 1:(i + 1) * num_steps
+                                       + 1])))
 
     return encoded_examples
 
 
 def create_data_loader_for_lm(batch_size, num_steps):
-    train_ds, valid_ds, test_ds = load_dataset('ptb',
-                                               splits=('train', 'valid',
-                                                       'test'))
+    train_ds, valid_ds, test_ds = load_dataset(
+        'ptb', splits=('train', 'valid', 'test'))
 
     train_examples = [
         train_ds[i]['sentence'].split() for i in range(len(train_ds))
     ]
     vocab = Vocab.build_vocab(train_examples, eos_token='</eos>')
 
-    trans_fn = partial(convert_ptb_example,
-                       vocab=vocab,
-                       batch_size=batch_size,
-                       num_steps=num_steps)
+    trans_fn = partial(
+        convert_ptb_example,
+        vocab=vocab,
+        batch_size=batch_size,
+        num_steps=num_steps)
     train_ds.map(trans_fn, batched=True)
     valid_ds.map(trans_fn, batched=True)
     test_ds.map(trans_fn, batched=True)
 
-    train_loader = paddle.io.DataLoader(train_ds,
-                                        return_list=True,
-                                        batch_size=None)
-    valid_loader = paddle.io.DataLoader(valid_ds,
-                                        return_list=True,
-                                        batch_size=None)
-    test_loader = paddle.io.DataLoader(test_ds,
-                                       return_list=True,
-                                       batch_size=None)
+    train_loader = paddle.io.DataLoader(
+        train_ds, return_list=True, batch_size=None)
+    valid_loader = paddle.io.DataLoader(
+        valid_ds, return_list=True, batch_size=None)
+    test_loader = paddle.io.DataLoader(
+        test_ds, return_list=True, batch_size=None)
     return train_loader, valid_loader, test_loader, len(vocab)
+
+
+def convert_sst2_example(example, vocab, num_steps):
+    """convert a example for lstm's input"""
+    input_ids = vocab.to_indices(example['sentence'].split()[:num_steps])
+    label = np.array(example['labels'], dtype="int64")
+    return input_ids, label
+
+
+def create_data_loader_for_text_classifier(batch_size, num_steps):
+    train_ds, valid_ds, test_ds = load_dataset(
+        'glue', 'sst-2', splits=('train', 'dev', 'test'))
+
+    train_examples = [
+        train_ds[i]['sentence'].split() for i in range(len(train_ds))
+    ]
+    vocab = Vocab.build_vocab(train_examples, eos_token='</eos>')
+
+    trans_func = partial(
+        convert_sst2_example, vocab=vocab, num_steps=num_steps)
+    train_ds = train_ds.map(trans_func)
+    valid_ds = valid_ds.map(trans_func)
+    test_ds = test_ds.map(trans_func)
+
+    batchify_fn = lambda samples, fn=Tuple(
+        Pad(axis=0),  # input
+        Stack(dtype="int64" if train_ds.label_list else "float32")  # label
+    ): fn(samples)
+
+    train_sampler = paddle.io.BatchSampler(
+        dataset=train_ds, batch_size=batch_size)
+    train_data_loader = paddle.io.DataLoader(
+        train_ds, batch_sampler=train_sampler, collate_fn=batchify_fn)
+    valid_sampler = paddle.io.BatchSampler(
+        dataset=valid_ds, batch_size=batch_size)
+    valid_data_loader = paddle.io.DataLoader(
+        valid_ds, batch_sampler=valid_sampler, collate_fn=batchify_fn)
+    test_sampler = paddle.io.BatchSampler(
+        dataset=test_ds, batch_size=batch_size)
+    test_data_loader = paddle.io.DataLoader(
+        test_ds, batch_sampler=test_sampler, collate_fn=batchify_fn)
+
+    return train_data_loader, valid_data_loader, test_data_loader, len(vocab)
--- data_loader_test.py
+++ data_loader_test.py
@@ -1,10 +1,9 @@
 import unittest
 
-from data_loader import create_data_loader_for_lm
+from data_loader import create_data_loader_for_lm, create_data_loader_for_text_classifier
 
 
 class TestDataLoader(unittest.TestCase):
-
     def test_create_data_loader_for_lm(self):
         BATCH_SIZE = 20
         NUM_STEPS = 35
@@ -23,6 +22,22 @@ class TestDataLoader(unittest.TestCase):
 
         # label[0] == data[1], label[1] == data[2], etc.
         assert (data[0][1:].equal_all(label[0][:-1]))
+
+    def test_create_data_loader_for_text_classifier(self):
+        BATCH_SIZE = 20
+        NUM_STEPS = 35
+        train_loader, valid_loader, test_loader, vocab_size = create_data_loader_for_text_classifier(
+            batch_size=BATCH_SIZE, num_steps=NUM_STEPS)
+
+        assert (vocab_size == 14817)
+
+        assert (len(train_loader) == 3368)
+        assert (len(valid_loader) == 44)
+        assert (len(test_loader) == 92)
+
+        data, label = next(train_loader())
+        assert (data.shape[0] == BATCH_SIZE)
+        assert (label.shape == [BATCH_SIZE])
 
 
 if __name__ == '__main__':
